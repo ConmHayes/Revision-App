e0568f6cea509d0de06f600a6d943de2f2d6eaf1
olgaKhristo <olga84h@gmail.com>
2023-10-30 16:10:28 +0000

    notes page done

--- client/src/pages/NotesPage/index.jsx
+++ client/src/pages/NotesPage/index.jsx
@@ -1,13 +1,55 @@
-//individual note page like time table( time tables)
-//add rute on app
-// import React, { useEffect, useState } from "react";
-
-// export default function NotesPage() {
-//   return <h1>Notes Page</h1>;
-// }
-
-import React from "react";
+// individual note page like time table( time tables)
+// add rute on app
+import React, { useEffect, useState } from "react";
+// import {}
 
 export default function NotesPage() {
-  return <div>Notes Page</div>;
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => { 
+    async function getNotes() {
+      const res = await fetch("http://localhost:3000/notes");
+      const notes = await res.json();
+      setNotes(notes);
+    }
+    getNotes();
+  }, []);
+
+  function displayNotes() {
+    return notes.map((note) => {
+      return (
+        <div key={note.id}>
+          <h1>{note.title}</h1>
+          <p>{note.description}</p>
+          <button onClick={deleteNote}>Delete</button>
+        </div>
+      );
+    });
+  }
+
+  async function deleteNote(id) {
+    const options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    await fetch(`http://localhost:3000/notes/${id}`, options);
+    const updatedNotes = notes.filter((note) => note.id !== id);
+    setNotes(updatedNotes);
+    
+  } 
+
+
+  
+  return (
+  <>
+
+
+  <h1>Notes Page</h1>
+  <div>{displayNotes()}</div>
+  </>
+
+  );
 }
+
