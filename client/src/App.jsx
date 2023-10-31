075dcfd1401dfbbc18d14a53ccfd01bd5cbd1d74
Connor Hayes <conmhayes@gmail.com>
2023-10-31 09:19:19 +0000

    fixed the import components error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import * as Pages from "./pages";
 import { Routes, Route } from "react-router-dom";
-import { Header } from "./Components";
+import { Header } from './components'
 
 function App() {
   return (
