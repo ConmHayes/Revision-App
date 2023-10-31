5401dd5958ae7635cd3931ec39dd65a0ec845cfb
David Evans <de121@ic.ac.uk>
2023-10-31 17:23:38 +0000

    Almost adding events

--- client/src/components/EventForm/index.jsx
+++ client/src/components/EventForm/index.jsx
@@ -1,7 +1,24 @@
 
 
-export default function EventForm({ tempData, setTempData }){
-    console.log(tempData)
+export default function EventForm({ tempData, setTempData, events, setEvents, createEvent, setCreateEvent }){
+    function handleCancel(){
+
+        setCreateEvent(false)
+    }
+
+    function handleCreate(e){
+        e.preventDefault()
+        setCreateEvent(false)
+    }
+    const subjects = ["ENGLISH", "MATHS", "SCIENCE", "LANGUAGES", "HISTORY", "GEOGRAPHY", "MUSIC", "ART", "TECHNOLOGY", "COMPUTING", "OTHER"]
+
+    function options(subjects){
+        const opines = subjects.map((s, idx) => <option className = "select-option" key = {idx} value = {s}>{s}</option>)
+
+        return opines
+    }
+
+
     return (
         <div className = "flexbox-container tall-form" style = {{height: "570px"}}>
             <div className="flexbox-header">
@@ -11,16 +28,23 @@ export default function EventForm({ tempData, setTempData }){
             <div className= "flex-options">
                 <input type = "checkbox" style = {{position: "relative", right: "5px"}}/>Revision 
                 <select style = {{  fontFamily: 'Courier New, Courier, monospace', position: "relative", left: "10px"}}>
-                    <option className="select-option" value = "English">ENGLISH</option>
-                    <option value = "Maths">MATHS</option>
-                    <option value = "Languages">LANGUAGES</option>
+                    {subjects.map((s, i) =>
+                        <option className = "select-option" key = {i}>{s}</option>
+                    )}
+                    
+                    
                 </select>
             </div> 
             <div className="flexbox-form"><textarea className="event-input" type = "text"></textarea></div>
             <div className="button-box">
-                <button className = "form-button"><i className="material-icons" style = {{color: "#ff724b", position: "relative", }}>close</i>Cancel</button><button className="form-button">Create Event</button>
+                <button className = "form-button" type = "click" onClick={handleCancel}><i className="material-icons" style = {{color: "#ff724b", position: "relative", top: "5px"}}>close</i>Cancel</button>
+                <button className = "form-button" type = "submit" onSubmit={handleCreate}><i className="material-icons" style = {{color: "#ff724b", position: "relative", top: "5px"}}>done</i>Create Event</button>
             </div>
             </form>
         </div>
     )
 }
+
+/*<option className="select-option" value = "English">ENGLISH</option>
+                    <option value = "Maths">MATHS</option>
+                    <option value = "Languages">LANGUAGES</option>*/
--- client/src/pages/TimetablesPage/index.jsx
+++ client/src/pages/TimetablesPage/index.jsx
@@ -4,6 +4,13 @@ import { useState, useEffect } from "react"
 export default function TimetablesPage() {
   const [createEvent, setCreateEvent] = useState(false)
   const [tempData, setTempData] = useState("")
+  const [events, setEvents] = useState({
+    id: 1,
+    date: new Date(),
+    description: "Revise Battle of Trafalgar",
+    subject: "History",
+    revision: true
+  })
 
 
 
@@ -13,7 +20,15 @@ export default function TimetablesPage() {
         <div className = "flex-item">
         <Calendar createEvent = { createEvent } setCreateEvent = { setCreateEvent } tempData = { tempData } setTempData={setTempData}/>
         </div>
-        <div className="flex-item" style = {{position: "relative", left: "75px"}}>{createEvent==true ? <EventForm tempData = {tempData} setTempData = {setTempData}/> : ""}</div>
+        <div className="flex-item" style = {{position: "relative", left: "75px"}}>
+          {createEvent==true ? <EventForm 
+                                    tempData = {tempData} 
+                                    setTempData = {setTempData}
+                                    events = {events}
+                                    setEvents = {setEvents}
+                                    createEvent = {createEvent}
+                                    setCreateEvent = {setCreateEvent}/> : ""}
+        </div>
       </div>;
     </>
 )
