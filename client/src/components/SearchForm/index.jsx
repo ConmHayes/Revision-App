69f1db3ab86b00eb1089b71a2a7a66eaf209b23b
olgaKhristo <olga84h@gmail.com>
2023-10-31 13:00:14 +0000

    started search form

--- client/src/components/SearchForm/index.jsx
+++ client/src/components/SearchForm/index.jsx
@@ -1,23 +1,24 @@
-// import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import React from "react";
 
-// export default function SearchForm() {
-// const [inputValue, setInputValue] = useState("")
-// const [searchResults, setSearchResults] = useState([])
-//   function handleInput(event) {
-//     console.log(event.target.value);
-//   }
-//     useEffect(() => {
-//         async function searchAPI() {
-//             const response = await fetch(`to be added&query=${inputValue}`)
-//             const data = await response.json()
-//             setSearchResults(data.results)
-//         }
+export default function SearchForm() {
+const [inputValue, setInputValue] = useState("")
+const [searchResults, setSearchResults] = useState([])
+  function handleInput(event) {
+    console.log(event.target.value);
+  }
+    useEffect(() => {
+        async function searchAPI() {
+            const response = await fetch(`to be added&query=${inputValue}`)
+            const data = await response.json()
+            setSearchResults(data.results)
+        }
 
-//     }, [inputValue])
-//   return (
-//     <form>
-//       <input type="text" placeholder="Search" onChange={handleInput} />
-//       <input type="submit" value="search" />
-//     </form>
-//   );
-// }
+    }, [inputValue])
+  return (
+    <form>
+      <input type="text" placeholder="Search" onChange={handleInput} />
+      <input type="submit" value="search" />
+    </form>
+  );
+}
