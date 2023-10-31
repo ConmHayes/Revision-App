f0dd72063b9ee1a0605f7c4b07485c820de0e0d1
olgaKhristo <olga84h@gmail.com>
2023-10-31 11:52:33 +0000

    testing for NotePage dome

---
+++ client/src/pages/NotePage/NotePage.test.jsx
@@ -0,0 +1,31 @@
+// testing done using jest and react-testing-library
+
+import React from "react";
+import { render, screen, waitFor, expect, describe, it, global, jest } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import NotePage from "./NotePage";
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve({ id: 1, title: "Note 1" }),
+  })
+);
+
+describe("NotePage", () => {
+  it("fetches and displays a note", async () => {
+    render(
+      <BrowserRouter>
+        <NotePage />
+      </BrowserRouter>
+    );
+
+   
+    await waitFor(() => {
+      expect(screen.getByText("Loading...")).not.toBeInTheDocument();
+    });
+
+    expect(screen.getByText("Note 1")).toBeInTheDocument();
+    expect(screen.getByText("Description")).toBeInTheDocument();
+  });
+});
