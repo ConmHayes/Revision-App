b87406baffcf4763118c9ace8a5ab996c1bfc813
olgaKhristo <olga84h@gmail.com>
2023-10-31 16:44:15 +0000

    NotesPage test

--- client/src/pages/NotesPage/NotesPage.test.jsx
+++ client/src/pages/NotesPage/NotesPage.test.jsx
@@ -1,37 +1,49 @@
 import React from "react";
-import { render, screen, cleanup} from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
 expect.extend(matchers);
 import { BrowserRouter } from "react-router-dom";
-import NotesPage from ".";
-import { afterEach, describe, expect, it, global, vi} from "vitest"
+import NotePage from "./NotePage"; 
+import { afterEach, describe, it, vi } from "vitest";
 
-describe("NotesPage", () => {
-    const fetchSpy = vi.spyOn(global, "fetch");
-    afterEach(() => {
-        cleanup();
-        fetchSpy.mockRestore();
-    });
+describe("NotePage", () => {
+  const fetchSpy = vi.spyOn(global, "fetch");
+  afterEach(() => {
+    cleanup();
+    fetchSpy.mockRestore();
+  });
 
-    it ("fetch notes", async () => {
-        const mockResponse = {            
-            "note_id": 1,
-            "note": "This is the note text 1",
-            "topic": "Topic 1"
-        }
-        const mockResolvedValue = {
-            ok: true,
-            json: () => new Promise(resolve => resolve(mockResponse))
-        }
-        fetchSpy.mockReturnValue(mockResolvedValue)
-        render(<BrowserRouter><NotesPage /></BrowserRouter>);
-        expect(fetch).toHaveBeenCalledTimes(1);
-        expect(fetch).toHaveBeenCalledWith("https://time-table-server.onrender.com/notes");
+  it("fetch note", async () => {
+    const mockResponse = {
+      "note_id": 1,
+      "note": "This is the note text 1",
+      "topic": "Topic 1"
+    };
+    const mockResolvedValue = {
+      ok: true,
+      json: () => new Promise((resolve) => resolve(mockResponse))
+    };
+    fetchSpy.mockReturnValue(mockResolvedValue);
 
-        const topic = await screen.findByText("Topic 1");
-        expect(topic).toBeInTheDocument();
-       
-    });
+   
+    render(
+      <BrowserRouter>
+        <NotePage />
+      </BrowserRouter>
+    );
 
+   
+    const topic = await screen.findByText("Topic 1");
+    expect(topic).toBeInTheDocument();
 
+   
+    const noteText = await screen.findByText("This is the note text 1");
+    expect(noteText).toBeInTheDocument();
+
+  
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(
+      "https://time-table-server.onrender.com/notes/1"
+    );
+  });
 });
