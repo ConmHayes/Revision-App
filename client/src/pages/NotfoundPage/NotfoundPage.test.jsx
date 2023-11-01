ed0596ba917c08933db98947956c2e4f5a2b865e
olgaKhristo <olga84h@gmail.com>
2023-11-01 11:46:01 +0000

    testing complete Co-authored-by: Connor Hayes <ConmHayes@users.noreply.github.com>

--- client/src/pages/NotfoundPage/NotfoundPage.test.jsx
+++ client/src/pages/NotfoundPage/NotfoundPage.test.jsx
@@ -3,7 +3,7 @@ import {screen, render, cleanup} from "@testing-library/react";
 import * as matchers from '@testing-library/jest-dom/matchers';
 expect.extend(matchers);
 import { BrowserRouter } from "react-router-dom";
-import { afterEach, beforeEach, describe, expect } from "vitest";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
 import NotfoundPage from ".";
 import userEvent from "@testing-library/user-event";
 
